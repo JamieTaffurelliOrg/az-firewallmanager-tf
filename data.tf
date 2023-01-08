b756f22b5f8c9f680c20e28c68babcdc2d931d92
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-01-08 20:43:30 +0000

    used better ip groups reference

--- data.tf
+++ data.tf
@@ -1,5 +1,3 @@
-data "azurerm_client_config" "current" {}
-
 data "azurerm_log_analytics_workspace" "logs" {
   provider            = azurerm.logs
   name                = var.log_analytics_workspace_name
